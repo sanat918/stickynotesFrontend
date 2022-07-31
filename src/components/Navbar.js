a6ef56f82bdc601aa85458fc93bc77a2468dd72c
sanat918 <97349793+sanat918@users.noreply.github.com>
2022-07-31 16:56:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar(props) {
   //for handling logout
     const navigate=useNavigate();
   const handleLogout=()=>{
-    localStorage.removeItem('token');
+    window.sessionStorage.removeItem('token');
     navigate("/Home")
     showAlert("Logout successfully","success")
   }    
@@ -19,21 +19,21 @@ function Navbar(props) {
     <div className="container-fullwidth" >
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-  <Link className="navbar-brand" to={localStorage.getItem('token')?"/NotesHome":"/Home"}>{props.title}</Link>
+  <Link className="navbar-brand" to={window.sessionStorage.getItem('token')?"/NotesHome":"/Home"}>{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarScroll">
       <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
       <li className="nav-item">
-       <Link className={`nav-link ${location.pathname==="/Home" ? "active":"" }`} aria-current="page" to={localStorage.getItem('token')?"/NotesHome":"/Home"}>Home</Link>
+       <Link className={`nav-link ${location.pathname==="/Home" ? "active":"" }`} aria-current="page" to={window.sessionStorage.getItem('token')?"/NotesHome":"/Home"}>Home</Link>
        </li>
         <li className="nav-item">
           <Link className={`nav-link ${location.pathname==="/About" ? "active":"" }`} aria-current="page" to="/About">About</Link>
         </li>
       </ul>
 
-               {!localStorage.getItem('token')?<form className="d-flex"> 
+               {!window.sessionStorage.getItem('token')?<form className="d-flex"> 
                     <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
                     <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
                     </form>:<button onClick={handleLogout} className='btn btn-primary'>Logout</button>}
